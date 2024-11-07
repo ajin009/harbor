9b4fdc9e7b2685b3be027d4f36c6664ad2c32da5
ajin009 <ajinwayanad@gmail.com>
2024-11-07 10:43:12 +0530

    removed

--- src/route/order.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { buyProduct,getUserOrders,getOrderById } = require('../controllers/order');
-const { auth } = require('../middleware/authorization'); 
-const router = express.Router();
-
-router.post('/place-order', auth, buyProduct);
-router.get('/', auth, getUserOrders); 
-router.get('/:orderId',auth, getOrderById); 
-module.exports = router;
